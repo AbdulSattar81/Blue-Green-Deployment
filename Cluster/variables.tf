befda0cf1f91a2be5db1d55a447baf30a4840fd1
Abdul Sattar <mabdulsattar785@gmail.com>
2025-12-10 02:59:01 -0600

    Update default SSH key name format

--- Cluster/variables.tf
+++ Cluster/variables.tf
@@ -1,6 +1,6 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "CI/CD_DevOps_Shack"
+  default     = "CI_CD_DevOps_Shack"
 }
 
